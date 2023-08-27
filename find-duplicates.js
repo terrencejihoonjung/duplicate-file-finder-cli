76668416429d3889914beb228367e736abbffad6
terrencejihoonjung <terrencejung928@gmail.com>
2023-08-27 16:23:40 -0700

    defined program and imported modules

--- find-duplicates.js
+++ find-duplicates.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+// Import modules
+const program = require("commander");
+const glob = require("glob");
+const sha1File = require("sha1-file");
+const path = require("path");
+const fs = require("fs");
+
+// Define program
+program
+  .version("1.0.0")
+  .arguments("<directory>") // acts upon argument input
+  .action((directory) => {
+    // takes argumnt and performs given action
+    findDuplicateFiles(directory);
+  })
+  .parse(process.argv);
